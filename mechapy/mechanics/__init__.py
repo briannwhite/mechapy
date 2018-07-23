1187bbddac01aba5054b0aedc267b693c87a8503
Brian White <brian.white@mining.komatsu>
2018-07-22 22:08:57 -0500

    Removed content from mechanics/__init__.py, as api is consolidated into root __init__.py

--- mechapy/mechanics/__init__.py
+++
@@ -1,6 +0,0 @@
-"""The 'mechanics' package contains content for numerous engineering mechanics topics"""
-
-from mechapy.mechanics.materials import CarbonSteel, StainlessSteel, CustomMetal, CustomPolymer,\
-    CustomMaterial, CarbonSteelRegistry, StainlessSteelRegistry, PolymerRegistry, BaseMetalRegistry
-from mechapy.mechanics.sections import Rectangle, Circle, RolledSection
-from mechapy.mechanics.statics import StressTensor
